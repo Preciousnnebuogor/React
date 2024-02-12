60619de4281da5a8c45885de7e3656ebdd0c2440
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-12 16:46:24 +0100

    learnt dynamic value

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  const title = "what a wonderful day to read"
+  const google = "www.google.com"
+
+  return (
+    <div className="">
+      <h1>{title}</h1>
+      <a href="{google}">site</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+export function Navbar(){
+    return <div>
+        <nav>
+            <h1>Welcome</h1>
+            <div>
+                <a href="/">Home</a>
+                <a href="/">New blog</a>
+            </div>
+        </nav>
+    </div>
+}
