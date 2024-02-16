2309caa507e4ca0c5a9e009f953b27dacfd959e7
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-17 00:44:51 +0100

    using useStste to increase and decrease a button

--- src/components/eventhandling.jsx
+++ src/components/eventhandling.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 export default function HandleEvent() {
-    function handleSubmit () {
-     console.log("i am okay")
-    }
-    return <div>
-        <button onCanPlay={handleSubmit}></button>
+  const [count, setCount] = useState(0);
+  const [count1, setCount2] = useState(0);
+
+  function handleSubmit() {
+    console.log("i am okay");
+    setCount(count + 1);
+  }
+
+  function handleSubmit2() {
+    console.log("i love you");
+    setCount2(count1 - 1);
+  }
+  return (
+    <div>
+      {count}
+      <button onClick={handleSubmit}>increment</button>
+      {count1}
+      <button onClick={handleSubmit2}>decrement</button>
     </div>
+  );
 }
