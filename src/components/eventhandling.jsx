b3f807d27d4044b40f193ed30b2a4dd831fd0286
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-17 01:10:26 +0100

    added event

--- src/components/eventhandling.jsx
+++ src/components/eventhandling.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 export default function HandleEvent() {
   const [count, setCount] = useState(0);
   const [count1, setCount2] = useState(0);
+  const [name, setName] = useState("");
 
   function handleSubmit() {
     console.log("i am okay");
@@ -12,12 +13,26 @@ export default function HandleEvent() {
     console.log("i love you");
     setCount2(count1 - 1);
   }
+
+  function handleChange(e) {
+    console.log("thank you");
+    setName(e.target.value);
+  }
   return (
     <div>
       {count}
       <button onClick={handleSubmit}>increment</button>
+      <br/>
       {count1}
       <button onClick={handleSubmit2}>decrement</button>
+      <br/>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          handleChange(e);
+        }}
+      />
     </div>
   );
 }
