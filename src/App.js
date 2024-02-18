edafad8f3cc8db5822d75dbd48b074467ff9c642
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-18 15:22:39 +0100

    added useEfect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import HandleEvent from "./components/eventhandling";
+import Effect from "./components/useEffect";
 
 function App() {
   return (
     <div>
       <HandleEvent/>
+      <Effect/>
     </div>
   );
 }
---
+++ src/components/useEffect.jsx
@@ -0,0 +1,19 @@
+import { useState,useEffect } from "react";
+export default function Effect() {
+  const [query, setQuery] = useState("pizza");
+  useEffect(()=>{
+    function presh(){
+        console.log("i am good")
+    }
+    presh()
+  }, [])
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+      />
+    </div>
+  );
+}
