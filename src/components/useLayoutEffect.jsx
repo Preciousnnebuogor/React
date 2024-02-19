d2f32d6b64032b1caa0c96c668f5d4de28c7b703
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-20 00:28:36 +0100

    compared useEffect and useLayoutEffect hooks

--- src/components/useLayoutEffect.jsx
+++ src/components/useLayoutEffect.jsx
@@ -1,9 +1,19 @@
-import {useLayoutEffect} from "react"
-export default function LayoutEffect(){
- useLayoutEffect(()=>{
-    console.log("i love you")
- },[])
-    return <div>
+import { useLayoutEffect, useRef, useEffect } from "react";
+export default function LayoutEffect() {
+  const inputRef = useRef();
 
+  useLayoutEffect(() => {
+    console.log(inputRef.current.value);
+  }, []);
+
+  useEffect(() => {
+    inputRef.current.value = "hello";
+  }, []);
+  return (
+    <div>
+      <input type="text" ref={inputRef}
+       value="Felix" />
     </div>
+  );
 }
+ 
