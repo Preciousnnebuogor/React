98471195131c5cb87700f6e968f12f9684649d33
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-19 23:57:01 +0100

    added useRefHook

--- src/components/useRef.jsx
+++ src/components/useRef.jsx
@@ -1,4 +1,13 @@
-import {useRef} from "react"
-export default function RefHook(){
-    return <div></div>
+import { useRef } from "react";
+export default function RefHook() {
+  const inputRef = useRef();
+  const onClick = () => {
+    console.log(inputRef.current.value);
+  };
+  return (
+    <div>
+      <input type="text" placeholder="cute..." ref={inputRef} />
+      <button onClick={onClick}></button>
+    </div>
+  );
 }
